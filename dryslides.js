2759635fb91cf92af91d906392eb9bd242117d9c
Andrew Maxwell <amaxwell01@gmail.com>
2012-07-05 18:53:06 -0700

    Finalized the scroller portion of the gallery

--- dryslides.js
+++ dryslides.js
@@ -7,7 +7,8 @@ var drySlide = function( args ) {
     var speed        = args.speed ? args.speed : 'slow';
     var startFrame   = args.startFrame ? args.startFrame : 0;
     var mainSlide    = args.mainSlide ? args.mainSlide : 0;
-    var middleSlide  = args.mainSlide ? Math.ceil(args.mainSlide / 2) : 0;
+    var beginningSlides = slideCount - mainSlide;
+    var firstChunk      = slideCount - beginningSlides;
     
      // Add the slide content to the content container
     var displayContent = function( slide ) {
@@ -19,6 +20,10 @@ var drySlide = function( args ) {
         // Show the content for the given slide
         $( contentItems + '[data-content="' + slide + '"]').fadeIn();
     };
+    
+    
+    // Set the data-middle attribute on the slides
+    slideContainer.attr('data-middle', mainSlide );
     
     // Iterate over all of the slide items and give them a data-slide number
     $.each(slideItems, function( index, value ) {
@@ -52,55 +57,55 @@ var drySlide = function( args ) {
     
     
     var previousSlide = function() {
-        var slideContainer = $('#drySlideContainer');
         var currentSlide   = parseInt(slideContainer.attr('data-current'));
         
         if( currentSlide > 0 )
         {
-            
-            if( currentSlide > mainSlide - 1 && 
-              ( currentSlide - 1 ) < ( slideCount - mainSlide )
-              )
-            {
-                $('#drySlides').animate({ 'left': '+=102px'}, speed );
-            }
-            
-            // Remove the select class from all items
-            $(slideItems).removeClass('selected');
-            
-            // Add the selected class to the previous slide           
-            $( slideItems[ currentSlide - 1 ] ).addClass('selected');
-            
-            slideContainer.attr('data-current', currentSlide - 1 );
+            slideContainer.attr('data-current', currentSlide);
+            selectSlideItem( currentSlide - 1, 'button' );
         }
     };
     
     
-    var selectSlideItem = function( currentSlide ) {
+    var selectSlideItem = function( currentSlide, call ) {
+        var middle = mainSlide - 1;
+        var middleSlide = parseInt( slideContainer.attr('data-middle') );
         
-        if( currentSlide < slideCount - 1 )
+        // Remove the select class from all items
+        $(slideItems).removeClass('selected');
+
+        // Add the selected class to the selected slide           
+        $( slideItems[ currentSlide ] ).addClass('selected');
+        
+        slideContainer.attr('data-current', currentSlide );
+        
+        if( currentSlide > (middleSlide - 1) )
         {
-            if( ( currentSlide + 1 ) > ( mainSlide - 1 ) && 
-                ( currentSlide ) < ( slideCount - mainSlide )
+            if( currentSlide < ( slideCount - ( mainSlide - 1) ) )
+            {
+                slideContainer.attr('data-middle', middleSlide + 1 );
+                $('#drySlides').animate({ 'left': '-=102px'}, speed );
+            }            
+        }
+        else
+        if( currentSlide < (middleSlide - 1) ) {
+            if( currentSlide >= (firstChunk - 1)
               )
             {
-                $('#drySlides').animate({ 'left': '-=102px'}, speed );
+                slideContainer.attr('data-middle', middleSlide - 1 );
+                $('#drySlides').animate({ 'left': '+=102px'}, speed );
             }
-           
-            // Remove the select class from all items
-            $(slideItems).removeClass('selected');
-
-            // Add the selected class to the next slide           
-            $( slideItems[ currentSlide + 1 ] ).addClass('selected');
-            
-            slideContainer.attr('data-current', currentSlide + 1 );
         }
-        
     };
     
     var nextSlide = function() {
-        var currentSlide   = parseInt(slideContainer.attr('data-current'));
-        selectSlideItem( currentSlide );
+        var currentSlide   = parseInt(slideContainer.attr('data-current')) + 1;
+        
+        if( currentSlide < slideCount )
+        {
+            slideContainer.attr('data-current', currentSlide + 1 );
+            selectSlideItem( currentSlide, 'button' );
+        }
     };
     
     
@@ -110,7 +115,7 @@ var drySlide = function( args ) {
         var that = $(this);
         var slide = parseInt( that.attr('data-slide') );
         
-        selectSlideItem( slide - 1 );
+        selectSlideItem( slide, 'click' );
         displayContent( slide );
     });
 };
